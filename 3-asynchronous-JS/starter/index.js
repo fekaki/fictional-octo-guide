bf8d5126f6c6a676082dd7ae63db104699fd7724
Felipe Arakaki <felipearakaki@outlook.com>
2022-10-16 23:37:07 -0300

    Lesson 44 Building Promisses

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,34 +1,36 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  // superagent  The Problem with Callbacks: Callback Hell
-    // não está puxando o ${data} quando substitui o hound do superagent.
-    // .get(`https://dog.ceo/api/breed/hound/images/random`)
-    // .end((err, res) => {
-    //   if (err) return console.log(err.message);
-    //   console.log(res.body.message);
-
-    //   fs.writeFile("dog-img.txt", res.body.message, err => {
-    //     if (err) return console.log(err.message);
-    //     console.log("Random dog image saved to file!");
-    //   });
-    // });
-
-    superagent // From Callback Hell To Promises
-    // não está puxando o ${data} quando substitui o hound do superagent.
-    .get(`https://dog.ceo/api/breed/hound/images/random`)
-    .then(res => {
-      console.log(res.body.message);
-
-      fs.writeFile("dog-img.txt", res.body.message, err => {
-        if (err) return console.log(err.message);
-        console.log("Random dog image saved to file!");
-      });
-    })
-    .catch(err => {
-      console.log(err.message);
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not FIND that file")
+      resolve(data);
     });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject("Could not WRITE the file");
+      resolve("Success!");
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+.then(data => {
+  console.log(`Breed: ${data}`);
+  return superagent.get(`https://dog.ceo/api/breed/hound/images/random`)
+})
+.then(res => {
+  console.log(res.body.message);
+  return writeFilePro("dog-img.txt", res.body.message)
+})
+.then(() => {
+  console.log("Random dog image saved to file!");
+})
+.catch(err => {
+  console.log(err);
 });
