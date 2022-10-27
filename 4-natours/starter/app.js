0c7305ada5efcf1ea489d9c7497c1330a245eb62
Felipe Arakaki <felipearakaki@outlook.com>
2022-10-27 10:50:47 -0300

    lesson 59 Creating Our Own Middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,18 +1,29 @@
 const fs = require('fs');
 const express = require('express');
-const { create } = require('domain');
 
 const app = express();
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware ✌');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.lenth,
     data: {
       tours
@@ -100,7 +111,6 @@ const deleteTour = (req, res) => {
 // app.post('/api/v1/tours', createTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
-
 
 app
   .route('/api/v1/tours')
