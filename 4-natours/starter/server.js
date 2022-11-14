41e58548e0e2cc508c491298e6a7a83a23285519
Felipe Arakaki <felipearakaki@outlook.com>
2022-11-14 15:17:42 -0300

    lesson 88 Refactoring for MVC

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
@@ -13,36 +13,11 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true, // new line add in commentary lesson 88
   }).then(() => console.log('DB connection successful!'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-const Tour = mongoose.model('Tour', tourSchema);
 
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997
-});
-
-testTour.save().then(doc => {
-  console.log(doc);
-}).catch(err => {
-  console.log('ERROR 😥😅:', err);
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
