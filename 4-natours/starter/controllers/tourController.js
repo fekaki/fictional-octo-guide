d5ee980053bc456a46cc163dd1616e555fd5f9c3
Felipe Arakaki <felipearakaki@outlook.com>
2023-01-12 13:04:01 -0300

    lesson 95 Making the API better: Filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,40 +1,55 @@
-const Tour = require('./../models/tourModel');
+const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    //const query = Tour.find()
+    //.where('duration')
+    //.equals(5)
+    //.where('difficulty')
+    //.equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query
+
+    // SEND RESPONSE
     res.status(200).json({
-      status: 'success',
+      status: "success",
       results: tours.length,
       data: {
-        tours
-      }
+        tours,
+      },
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
-      message: err
+      status: "fail",
+      message: err,
     });
   }
 };
 
 exports.getTour = async (req, res) => {
- try {
+  try {
     const tour = await Tour.findById(req.params.id);
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
-        tour
-      }
+        tour,
+      },
     });
- } catch (err) {
-  res.status(404).json({
-    status: 'fail',
-    message: err
-  });
- }
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -45,15 +60,15 @@ exports.createTour = async (req, res) => {
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       data: {
-        tour: newTour
-      }
+        tour: newTour,
+      },
     });
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
-      message: err
+      status: "fail",
+      message: err,
     });
   }
 };
@@ -62,37 +77,35 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true
-    })
+      runValidators: true,
+    });
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
-        tour
-      }
+        tour,
+      },
     });
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent!'
+      status: "fail",
+      message: "Invalid data sent!",
     });
   }
-
 };
 
 exports.deleteTour = async (req, res) => {
   try {
-    await Tour.findByIdAndDelete(req.params.id)
+    await Tour.findByIdAndDelete(req.params.id);
 
     res.status(204).json({
-      status: 'success',
-      data: null
+      status: "success",
+      data: null,
     });
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent!'
+      status: "fail",
+      message: "Invalid data sent!",
     });
   }
-
 };
