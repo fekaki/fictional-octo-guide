41b2a305b45296374ca91b0a8c9bf8b21f0fed17
Felipe Arakaki <felipearakaki@outlook.com>
2023-01-24 17:20:50 -0300

    lesson 104 Virtual Properties

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,59 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name'],
+    required: [true, "A tour must have a name"],
     unique: true,
-    trim: true
+    trim: true,
   },
   duration: {
     type: Number,
-    required: [true, 'A tour must have a duration']
+    required: [true, "A tour must have a duration"],
   },
   maxGroupSize: {
     type: Number,
-    required: [true, 'A tour must have a group size']
+    required: [true, "A tour must have a group size"],
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, "A tour must have a difficulty"],
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
   ratingsQuantity: {
     type: Number,
-    default: 0
+    default: 0,
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price']
+    required: [true, "A tour must have a price"],
   },
   priceDiscount: Number,
   summary: {
     type: String,
     trim: true,
-    required: [true, 'A tour must have a description']
+    required: [true, "A tour must have a description"],
   },
   description: {
     type: String,
-    trim: true
+    trim: true,
   },
   imageCover: {
     type: String,
-    required: [true, 'A tour must have a cover image']
+    required: [true, "A tour must have a cover image"],
   },
   images: [String],
   createAt: {
     type: Date,
     default: Date.now(),
-    select: false
+    select: false,
   },
-  startDates: [Date]
+  startDates: [Date],
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
-const Tour = mongoose.model('Tour', tourSchema);
+tourSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
