0f73fc6f12fdcbf4aa6f2ea5fc75edb1e93f2cc2
Felipe Arakaki <felipearakaki@outlook.com>
2023-01-29 18:15:17 -0300

    lesson 112 Handling Unhandled Routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,26 +1,20 @@
+const express = require("express");
+const morgan = require("morgan");
 
-const express = require('express');
-const morgan = require('morgan');
-
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 // 1) MIDDLEWARES
 // console.log(process.env.NODE_ENV);
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-};
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware ✌');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -29,7 +23,14 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
