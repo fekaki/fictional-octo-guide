a7d92c1afd67e692cb203342ba11dfbd0fbc8f85
Felipe Arakaki <felipearakaki@outlook.com>
2023-02-13 17:52:43 -0300

    lesson 144 Setting Security HTTP Headers

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -10,11 +11,16 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
-// console.log(process.env.NODE_ENV);
+// set security HTTP headres
+app.use(helmet())
+
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -22,10 +28,16 @@ const limiter = rateLimit({
 });
 app.use("/api", limiter);
 
+
+// Body parser, reading data from bpdy into req.body
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
