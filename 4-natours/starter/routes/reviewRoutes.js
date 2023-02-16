18a7f19f0e97ba5e0dc6d8a9d6683fdd72cc0233
Felipe Arakaki <felipearakaki@outlook.com>
2023-02-16 17:16:51 -0300

    lesson 160 Adding a Nested GET Endpoint

--- 4-natours/starter/routes/reviewRoutes.js
+++ 4-natours/starter/routes/reviewRoutes.js
@@ -5,6 +5,7 @@ const authController = require("./../controllers/authController");
 const router = express.Router({ mergeParams: true });
 
 // POST /tour/234fa4/reviews
+// GET /tour/234fa4/reviews
 // POST /reviews
 
 router
