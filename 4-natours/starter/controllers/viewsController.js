02e44fd2e645e4258500543aae06ba4ef649b3fc
Felipe Arakaki <felipearakaki@outlook.com>
2023-03-03 16:11:11 -0300

    lesson 186 Including a Map with Mapbox - Part 1

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -25,7 +25,7 @@ exports.getTour = catchAsync(async (req, res) => {
   //3) render tempalte using the date from 1)
 
   res.status(200).render("tour", {
-    title: "The Forest Hiker Tour",
+    title: `${tour.name} Tour`,
     tour,
   });
 });
