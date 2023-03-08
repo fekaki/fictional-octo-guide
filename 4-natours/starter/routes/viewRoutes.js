df01db078ce3fa75133b47f33260bc55cbb46476
Felipe Arakaki <felipearakaki@outlook.com>
2023-03-08 12:39:53 -0300

    lesson 190 Logging in User with Our API - Part 2

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -4,10 +4,10 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get("/", viewsController.getOverview);
-router.get("/tour/:slug", authController.protect, viewsController.getTour);
-router.get("/login", viewsController.getLoginForm);
+router.use(authController.isLoggedIn);
 
-// /login
+router.get("/", viewsController.getOverview);
+router.get("/tour/:slug", viewsController.getTour);
+router.get("/login", viewsController.getLoginForm);
 
 module.exports = router;
