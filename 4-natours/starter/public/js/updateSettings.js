6ae0f75af484cc13860a1674e593b6cb93e5e011
Felipe Arakaki <felipearakaki@outlook.com>
2023-03-16 11:54:41 -0300

    lesson 196 Updating User Data with Our API

---
+++ 4-natours/starter/public/js/updateSettings.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import axios from "axios";
+import { showAlert } from "./alerts";
+
+export const updateData = async (name, email) => {
+  try {
+    const res = await axios({
+      method: "PATCH",
+      url: "http://localhost:3000/api/v1/users/updateMe",
+      data: {
+        name,
+        email,
+      },
+    });
+
+    if (res.data.status === "success") {
+      showAlert("success", "Data updated in successfully");
+      window.setTimeour(() => {
+        location.assign("/");
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert("error", err.response.data.message);
+  }
+};
