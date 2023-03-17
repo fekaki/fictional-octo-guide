e9057d53008a7ac1b00a93131bd142c9a5642521
Felipe Arakaki <felipearakaki@outlook.com>
2023-03-17 17:18:17 -0300

    lesson 201 Saving Image Name to Database

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -43,9 +43,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
-
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -57,8 +54,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
-  const filterBody = filterObj(req.body, "name", "email");
-  const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+  const filteredBody = filterObj(req.body, "name", "email");
+  if (req.file) filteredBody.photo = req.file.filename;
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true,
   });
