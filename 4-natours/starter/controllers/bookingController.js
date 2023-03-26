b2503a53ece2a0f5b9488652d9d6d947978211ec
Felipe Arakaki <felipearakaki@outlook.com>
2023-03-26 10:12:42 -0300

    lesson 212 Processing Payrments on the Front End

--- 4-natours/starter/controllers/bookingController.js
+++ 4-natours/starter/controllers/bookingController.js
@@ -10,28 +10,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   // 2) Create Checkout session
   const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
-  // const session = await stripe.checkout.sessions.create({
-  //   payment_method_types: ["card"],
-  //   success_url: `${req.protocol}://${req.get("host")}`,
-  //   cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
-  //   customer_email: req.user.email,
-  //   client_reference_id: req.params.tourId,
-  //   line_items: [
-  //     {
-  //       quantity: 1,
-  //       price_data: {
-  //         currency: "usd",
-  //         unit_amount: tour.price,
-  //         product_data: {
-  //           name: `${tour.name} Tour`,
-  //           description: tour.summary,
-  //           images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
-  //         },
-  //       },
-  //     },
-  //   ],
-  //   mode: "payment",
-  // });
+
   const session = await stripe.checkout.sessions.create({
     //session info
     payment_method_types: ["card"],
---
+++ 4-natours/starter/public/js/stripe.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+import axios from "axios";
+import { showAlert } from "./alerts";
+const stripe = Stripe(
+  "pk_test_51MopphGv05gFj0Vru18gOcZ6IIlW6MwCU7Wih87aY6JIGqhvjpXat1Ul0eFatNSrcfBV7kgfiCA2rTsVLMhQbFWw003dyH27NW"
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert("error", err);
+  }
+};
